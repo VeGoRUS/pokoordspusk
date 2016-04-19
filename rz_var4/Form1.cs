d608f5f1cb7b16310ebb378ad69f3499b32a6aab
VeGoRUS <gucvit@yandex.ru>
2016-04-19 08:03:50 +0300

    Начало разработки программы

---
+++ rz_var4/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rz_var4
+{
+    public partial class Form1 : Form
+    {
+        double x1, x2;
+        double fun;
+        int i;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+            }
+            catch(FormatException)
+            {
+
+            }
+        }
+        
+        private double f(tx1,tx2)
+        {
+            return ;
+        }
+    }
+}
